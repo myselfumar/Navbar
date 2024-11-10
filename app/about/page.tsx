0f98563f5f1c07a1a12ccd7f8ef550dcb2df5c1e
Muhammad Umar <umar.dms@gmail.com>
2024-11-10 22:00:54 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function About(){
+    return(
+        <h6>My Name Is Muhammad Umar </h6>
+    )
+}
---
+++ app/contact-us/page.tsx
@@ -0,0 +1,8 @@
+export default function contactme(){
+    return(
+        <div>
+        <h2>microsoft@hotmail.com</h2>
+        <h1>abcxyz@gmail.com</h1>
+        </div>
+    );
+   }
